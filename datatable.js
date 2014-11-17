ffce13e1fe36bd234de88724fd45ea509e406fc2
JesseChrestler <JesseChrestler@gmail.com>
2014-11-17 15:46:10 -0500

    added support for ordering columns with data-format, and also added support for data-sort. data-sort allows you to specify which columns in which order that you want to sort. example: data-sort="lastName, firstName"

--- datatable.js
+++ datatable.js
@@ -1,47 +1,71 @@
 (function($, angular, window){
-    'use strict';
-    angular.module('app.datatable', []).directive('table', function ($parse, $compile, $timeout) {
+    "use strict";
+    angular.module("app.datatable", []).directive("table", function ($parse, $compile, $timeout) {
         return {
-            restrict: 'A',
+            restrict: "A",
             //share scope with parent
             scope : false,
-            transclude : true,
             //directive main scope.
-            link: function postLink(scope, element, attrs, ctrl, transclude) {
-                transclude(scope, function(clone, scope) {
-                    element.append(clone);
-                });
-                //set a default if one isn't defined.
+            link: function postLink(scope, element, attrs, ctrl) {
+
+                //set a default if one isn"t defined.
                 if(angular.isUndefined(attrs.table)){
-                    attrs.table = 'table';
+                    attrs.table = "table";
                 }
 
-                //unfortunately element doesn't work so i have to convert it to a jquery object
+                //unfortunately element doesn"t work so i have to convert it to a jquery object
                 var $element = $(element[0]);
-                if(!$element.is('table')){
-                    throw new Error('Datatable can only be added to a table ex: <table data-table="myTable">');
+                if(!$element.is("table")){
+                    throw new Error("Datatable can only be added to a table ex: <table data-table='myTable'>");
+                }
+                var regex = {
+                    token : /\{\{([^}{]+)\}\}/g,
+                    name : /[\{\}]/g,
+                    filter: /[\s]*[^"']*["']*([^"' |]+).+/
                 }
                 //determines the columns based on the names.
                 function getColumns(){
-                    return $element.find('th').map(function(){
+                    return $element.find("th").map(function(){
                         var column = $(this);
-                        var columnData = column.data('column');
-                        var classData = column.data('class');
-                        var isVisible = !column.hasClass('hidden');
-                        var isSortable = column.data('sortable')  !== 'false';
-                        var isOrderable = column.data('orderable') !== 'false';
-                        var format = column.data('format');
-                        var formatFunctionName = column.data('formatfunction');
-                        var defaultContent = '';
+                        var columnData = column.data("column");
+                        var classData = column.data("class");
+                        var isVisible = !column.hasClass("hidden");
+                        var isSortable = column.data("sortable")  !== "false";
+                        var isOrderable = column.data("orderable") !== "false";
+                        var format = column.data("format");
+                        var formatFunctionName = column.data("formatfunction");
+                        var defaultContent = "";
                         if(columnData === undefined){
-                            isSortable = false;
-                            isOrderable = false;
-                            if(column.text() === '' && angular.isUndefined(format) && angular.isUndefined(formatFunctionName)){
-                                defaultContent = '';
+                            //isOrderable = false;
+                            //isSortable = false;
+                            if(column.text() === "" && angular.isUndefined(format) && angular.isUndefined(formatFunctionName)){
+                                defaultContent = "";
                             }
+                            var fields = [];
+                            if(column.data("sort") !== undefined){
+                                fields = column.data("sort").split(",");
+                            }else{
+                                if(format !== undefined){
+                                    var matches = format.match(regex.token);
+                                    if(matches !== null){
+                                        for(var i = 0; i < matches.length;i++){
+                                            var match = matches[i];
+                                            var name = match.replace(regex.name, "");
+                                            if(name.indexOf('|') !== -1){
+                                                name = name.replace(regex.filter, '$1');
+                                            }
+                                            if(name !== "index"){
+                                                fields.push(name);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            
                         }
                         return {
                             data : columnData,
+                            sort : fields,
                             defaultContent : defaultContent,
                             visible : isVisible,
                             sortable : isSortable,
@@ -52,13 +76,37 @@
                 }
                 //base call for ajax might need to prune the search data to a simple object.
                 function ajax(serverData, callback, settings){
+                    var options = {
+                        offset : serverData.start,
+                        limit : serverData.length
+                    }
+                    if(serverData.search.value !== ''){
+                        options.search = serverData.search.value;
+                    }
+                    var order = [];
+                    for(var i = 0; i < serverData.order.length;i++){
+                        var orderBy = serverData.order[i];
+                        var column = serverData.columns[orderBy.column];
+                        var config = columnConfig[orderBy.column];
+                        if(column.orderable){
+                            if(angular.isDefined(config.sort) && config.sort.length > 0){
+                                order.push(config.sort.join(' ' + orderBy.dir + ', ') + ' ' + orderBy.dir);
+                            } else if(angular.isDefined(column.data) && column.data !== orderBy.column){
+                                order.push(column.data + ' ' + orderBy.dir);
+                            }
+                        }
+                    }
+                    if(order.length > 0){
+                        options.sort = order.join(', ');
+                    }
+
                     var getDataSource = scope[attrs.source];
-                    if(angular.isDefined(getDataSource) && typeof getDataSource === 'function' ){
-                        scope[attrs.source](serverData, processData(callback), settings);
+                    if(angular.isDefined(getDataSource) && typeof getDataSource === "function" ){
+                        scope[attrs.source](options, processData(callback, serverData), settings);
                     }
                 }
                 //all data will be processed in this function
-                function processData(callback){
+                function processData(callback, serverData){
                     return function(data){
                         //here we can evaluate the data from the callback before we pass it to the datatables.
                         var response;
@@ -74,24 +122,25 @@
                         else{
                             response = data;
                         }
+                        response.draw = serverData.draw;
                         callback(response);
                     };
                 }
-                //rows that are created call this. this let's us do some cool stuff on a per row basis
+                //rows that are created call this. this let"s us do some cool stuff on a per row basis
                 function processRow(row, data, index){
-                   var columns = $(row).find('td');
+                   var columns = $(row).find("td");
                    for(var i = 0; i < columns.length;i++){
                         var column = $(columns[i]);
-                        var header = $element.find('th').eq(i);
+                        var header = $element.find("th").eq(i);
                         //we need to figure out if we have a format
-                        var format = header.data('format');
-                        var formatFunctionName = header.data('formatfunction');
+                        var format = header.data("format");
+                        var formatFunctionName = header.data("formatfunction");
                         if(angular.isDefined(format) || angular.isDefined(formatFunctionName)){
                             var formatFunction = scope[formatFunctionName];
-                            if(angular.isDefined(formatFunction) && typeof formatFunction === 'function'){
+                            if(angular.isDefined(formatFunction) && typeof formatFunction === "function"){
                                 column.html(formatFunction(row, data, index));
                             }else{
-                                column.html(format);    
+                                column.html(format);
                             }
                         }
                    }
@@ -102,12 +151,22 @@
                 }
                 function applyTokens(row, data){
                     var html = row.innerHTML;
-                    var matches = html.match(/\{\{([^}]+)\}\}/g);
+                    var matches = html.match(regex.token);
                     if(matches !== null){
                         for(var i = 0; i < matches.length;i++){
+
                             var match = matches[i];
-                            var name = match.replace(/[\{\}]/g, '');
+                            var name = match.replace(regex.name, "");
+                            var isFilter = false;
+                            if(name.indexOf('|') !== -1){
+                                //must be a filter.
+                                isFilter = true;
+                                name = name.replace(regex.filter, '$1');
+                            }
                             var val = data[name];
+                            if(isFilter){
+                                val = match.replace(name, val);
+                            }
                             if(angular.isDefined(val)){
                                 html = html.replace(match, val);
                             }
@@ -117,39 +176,40 @@
                 }
 
                 //instantiate the datatable plugin with all configuration options
+                var columnConfig = getColumns();
                 var dt = $element.DataTable({
-                    //ajax: 
+                    //ajax:
                     //dom : template,
-                    processing : true,
+                   // processing : true,
                     serverSide : true,
                     ajax : ajax,
-                    columns:getColumns(),
+                    columns:columnConfig,
                     rowCallback:processRow
                 });
-                
-                var table = $('[data-table="' + attrs.table + '"]');
+
+                var table = $("[data-table='" + attrs.table + "']");
                 var lastSelected;
                 //live binding for any elements added after rendering.
                 //this event handles all section types (shift, control and regular click)
-                table.on('click', 'tr', function(e){
+                table.on("click", "tr", function(e){
                     var shiftKey = e.shiftKey;
                     var ctrlKey = e.ctrlKey;
-                    var isMultipleSelect = attrs.selection === 'multiple';
+                    var isMultipleSelect = attrs.selection === "multiple";
 
                     if(!isMultipleSelect || (!shiftKey && !ctrlKey)){
-                        var selected = table.find('.selected');
-                        if(selected.length == 1 && $(this).hasClass('selected')){
-                            $(this).removeClass('selected');
+                        var selected = table.find(".selected");
+                        if(selected.length == 1 && $(this).hasClass("selected")){
+                            $(this).removeClass("selected");
                         }else{
-                            selected.removeClass('selected');
-                            $(this).addClass('selected');
+                            selected.removeClass("selected");
+                            $(this).addClass("selected");
                         }
                     }else if(isMultipleSelect && shiftKey){
-                        //this is to clear the selection range so that we dont' accidently select the text while changing selection
+                        //this is to clear the selection range so that we dont" accidently select the text while changing selection
                         window.document.getSelection().removeAllRanges();
                         if(angular.isDefined(lastSelected)){
-                            var lastIndex = table.find('tr').index(lastSelected);
-                            var thisIndex = table.find('tr').index(this);
+                            var lastIndex = table.find("tr").index(lastSelected);
+                            var thisIndex = table.find("tr").index(this);
                             var start = lastIndex+1;
                             var end = thisIndex;
                             if(lastIndex > thisIndex){
@@ -157,30 +217,29 @@
                                 end = lastIndex-1;
                             }
                             if(lastIndex !== thisIndex){
-                                var rows = table.find('tr');
+                                var rows = table.find("tr");
                                 for(var i = start; i <= end;i++){
-                                    $(rows[i]).toggleClass('selected');
+                                    $(rows[i]).toggleClass("selected");
                                 }
                             }else{
-                                $(this).toggleClass('selected');
+                                $(this).toggleClass("selected");
                             }
                         }else{
-                            $(this).toggleClass('selected');
+                            $(this).toggleClass("selected");
                         }
-                        
+
                     }else if (isMultipleSelect && ctrlKey){
-                        $(this).toggleClass('selected');
+                        $(this).toggleClass("selected");
                     }
                     lastSelected = $(this);
                 });
-
                 //expose functions to controller (we only want to expose certain things)
                 scope[attrs.table] = {
                     refresh : function(){
                         dt.ajax.reload();
-                    }, 
+                    },
                     getSelected : function(){
-                        return dt.rows('.selected').data();
+                        return dt.rows(".selected").data();
                     },
                     setColumnVisibility:function(index, isVisible){
                         return dt.column(index).visible(isVisible);
--- test/app/app.js
+++ test/app/app.js
@@ -1,49 +1,31 @@
 (function(){
   "use strict";
   angular.module( "appSample", ['app.datatable'])
-  .controller( "AppController", function AppCtrl ( $scope ) {
+  .controller( "AppController", function AppCtrl ( $scope, $http ) {
     $scope.reload=function(){
       //$scope.myTable.reload();
       $scope.myTable.reload();
     };
     $scope.getGridData = function(serverData, update){
-      update([{
-        "Name":"Tiger Nixon",
-        "Position" : "System Architect",
-        "Office" : "Edinburgh", 
-        "Age" : "61",
-        "StartDate" : "2011/01/12",
-        "Salary" : "$120,800"
-      },{
-        "Name":"John Doe",
-        "Position" : "Architect",
-        "Office" : "New York", 
-        "Age" : "42",
-        "StartDate" : "2012/06/25",
-        "Salary" : "$220,800"
-      },{
-        "Name":"Dave Miller",
-        "Position" : "BA",
-        "Office" : "Charlotte", 
-        "Age" : "33",
-        "StartDate" : "2008/04/12",
-        "Salary" : "$75,800"
-      }]);
+      $http.get("app/data.json").success(function(response){
+        update(response);
+      })
+      
     };
 
     $scope.getTestData = function(serverData, update){
       update([{
         "id":1,
         "Name" : "Tom",
-        "Salary" : "$120,800"
+        "Salary" : "55432.24"
       },{
         "id":2,
         "Name" : "Joe",
-        "Salary" : "$220,800"
+        "Salary" : "34033.54"
       },{
         "id":3,
         "Name" : "Bob",
-        "Salary" : "$75,800"
+        "Salary" : "22123"
       }]);
     };
   });
